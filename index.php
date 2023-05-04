8cde5a20dcd24380c317d064bb369c175e5d77d4
Nivsd <nivashampm@gmail.com>
2023-05-04 15:13:31 +0530

    person changed

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </head>
 <body>
      <form action="col.php" method="post">
-Name: <input type="text" name="name " ><br>
+person: <input type="text" name="name" ><br>
 D.O.B: <input type="date" id="birthday" name="birthday"><br>
 ph.no:<input type="tel" id="phone" name="phone" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required><br>
 upload photo:<input type="file" id="myFile" name="filename"><br>
